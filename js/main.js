1ae87ab6be005a946502df52059227a439fe82aa
Marina51533 <marina51533@gmail.com>
2020-09-26 15:13:47 +0200

    new positions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks',
+    product: 'New Car',
     image:'./img/bmw.jpg',
     inStock:10,
-    details:['red','auto','BMW']
+    details:['red','auto','BMW'],
+    variants:[
+    {
+variantId:2233,
+variantColor:'red',
+variantImage:'./img/bmw.jpg',
+    },
+    {
+      variantId:28933,
+     variantColor:'blue',
+     variantImage:'./img/car_red.jpg',
+    }
+    ],
+    cart:0,
+  },
+
+  methods:{
+    addToCart: function (){
+      this.cart +=1
+    },
+    updateProduct(variantImage){
+      this.image=variantImage
+    }
   }
 })
