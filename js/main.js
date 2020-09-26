c1e60ee72e6301167ea721b0dfb2456fa7b2c4ed
Marina51533 <marina51533@gmail.com>
2020-09-26 17:02:21 +0200

    new options

--- js/main.js
+++ js/main.js
@@ -1,31 +1,46 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Lous Vettone',
     product: 'New Car',
-    image:'./img/bmw.jpg',
-    inStock:0,
+    selectedVariant:0,
     details:['red','auto','BMW'],
     variants:[
     {
 variantId:2233,
 variantColor:'red',
 variantImage:'./img/bmw.jpg',
+variantQuantity:10
     },
     {
       variantId:28933,
      variantColor:'blue',
      variantImage:'./img/car_red.jpg',
+     variantQuantity:0
     }
     ],
     cart:0,
   },
+   
+computed:{
+  title(){
+    return this.brand + ' '+this.product
+  },
+  image() {
+    return this.variants[this.selectedVariant].variantImage
+  },
+  inStock() {
+return this.variants[this.selectedVariant].variantQuantity
+  }
+},
 
   methods:{
     addToCart: function (){
       this.cart +=1
     },
-    updateProduct(variantImage){
-      this.image=variantImage
+    updateProduct(index){
+      this.selectedVariant=index
+   
     }
   }
 })
