293024c9e319f0f066ef7d21b6121dafdef4e531
Marina51533 <marina51533@gmail.com>
2020-09-26 14:34:32 +0200

    new dates

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image:'./img/bmw.jpg'
+    image:'./img/bmw.jpg',
+    inStock:10,
+    details:['red','auto','BMW']
   }
 })
