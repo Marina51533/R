ca568d125165db14dfc626f0f252fb9c78b64d60
Marina51533 <marina51533@gmail.com>
2020-09-27 21:28:31 +0200

    new conditionals

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const app= new Vue({
       product: 'Cars',
       describtion: ' The most popular cars',
       image: './img/green.jpg',
-      anchor: 'https://www.youtube.com/watch?v=b6Ac0jcqJIg&t=446s'
+      anchor: 'https://www.youtube.com/watch?v=b6Ac0jcqJIg&t=446s',
+      inStock:false,
       
     
   }
