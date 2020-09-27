3eb26256156640c2fe0f9f77c8c58208b00ef195
Marina51533 <marina51533@gmail.com>
2020-09-27 20:14:50 +0200

    first step

--- js/main.js
+++ js/main.js
@@ -1,46 +1,11 @@
-var app = new Vue({
+const app= new Vue({
   el: '#app',
-  data: {
-    brand: 'Lous Vettone',
-    product: 'New Car',
-    selectedVariant:0,
-    details:['red','auto','BMW'],
-    variants:[
-    {
-variantId:2233,
-variantColor:'red',
-variantImage:'./img/bmw.jpg',
-variantQuantity:10
-    },
-    {
-      variantId:28933,
-     variantColor:'blue',
-     variantImage:'./img/car_red.jpg',
-     variantQuantity:0
-    }
-    ],
-    cart:0,
-  },
+  data:{
    
-computed:{
-  title(){
-    return this.brand + ' '+this.product
-  },
-  image() {
-    return this.variants[this.selectedVariant].variantImage
-  },
-  inStock() {
-return this.variants[this.selectedVariant].variantQuantity
-  }
-},
-
-  methods:{
-    addToCart: function (){
-      this.cart +=1
-    },
-    updateProduct(index){
-      this.selectedVariant=index
-   
-    }
+      product: 'Cars',
+      describtion: ' The most popular cars'
+      
+      
+    
   }
 })
